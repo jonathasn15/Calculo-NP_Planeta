ee327323a0cd694b7e145b4b3eaf3bfd4cf0b857
Jonathas Nunes <jonathas.ns1509@gmail.com>
2022-11-02 13:54:02 -0300

    Update index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -57,7 +57,7 @@ export const GridItem = ({ item }: Props) => {
          {item.title === 'Faixa 4' &&
         <div className={styles.yorImc}>
          Valor MÁXIMO do desconto: R${item.levelCopy.descP50}<br/>
-         Valor MINIMO a pagar: R$ <strong>50,00</strong><br/>
+         Valor pagar: R$ <strong>50,00</strong><br/>
          Valor MINIMO do boleto: R$ <strong>51,00</strong>
         </div>
         }
