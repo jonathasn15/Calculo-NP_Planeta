d3bd87fce26e4c57fe9febe7fd92bceb960277d7
Jonathas Nunes <jonathas.ns1509@gmail.com>
2022-11-07 15:41:18 -0300

    Update index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -59,12 +59,13 @@ export const GridItem = ({ item }: Props) => {
         }
                     
         {item.title === 'Faixa 4' &&
+            
                 <div className={styles.yorImc}>
                 Valor MÁXIMO do desconto: R${item.descP100}<br/>
                 Valor pagar: R$ <strong>100,00</strong><br/>
                 Valor do boleto: R$ <strong>101,00</strong>
-                </div>
-        }             
+                </div>}
+                    
         </div>
         
         }
@@ -77,4 +78,3 @@ export const GridItem = ({ item }: Props) => {
         </div>
     );
 }
-
