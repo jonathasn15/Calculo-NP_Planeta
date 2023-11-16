940bc350030f0d82e5f8aeaa52a4e7512f95fcb8
Jonathas Nunes <jonathas.ns1509@gmail.com>
2023-11-15 22:26:13 -0300

    Update index.tsx
    Atualização NP 15/11/2023

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -23,14 +23,31 @@ export const GridItem = ({ item }: Props) => {
         </div>
         {item.atraso && 
         <div className={styles.yorImc}> Dias em atraso: {item.atraso} dias<br/>
-        NP permite: <strong>{item.desconto}</strong><br/>
+        NP Atualização: <strong>{item.descontoAtualização}</strong><br/>
+        NP Quitação: <strong>{item.desconto}</strong><br/>
         Valor Total: R$ {item.valorBase?.toFixed(2)}<br/>
         
         {item.title === 'Faixa 0' &&
         <div className={styles.yorImc}>
+            <hr />
+            <br/>
+            <p>Atualização:</p>
+            {item.descTotalAtual === 0 ? (
+                <p>para esse atraso não é possivel atualizar</p>
+            ) : (
+            <div>
+                Valor do desconto: R${item.descTotalAtual?.toFixed(2)}<br/>
+                Valor MINIMO a pagar: R$ <strong>{item.valorPayAtual?.toFixed(2)}</strong><br/>
+            </div>
+            )}
+
+            <br/>
+            <hr />
+            <br/>
+            <p>Quitação:</p>
             Valor do desconto: R${item.descTotal?.toFixed(2)}<br/>
             Valor MINIMO a pagar: R$ <strong>{item.valorPay?.toFixed(2)}</strong><br/>
-            Valor do boleto: R$ <strong>{item.valorBol?.toFixed(2)}</strong>
+
         </div>
         }
                     
@@ -38,23 +55,29 @@ export const GridItem = ({ item }: Props) => {
         <div className={styles.yorImc}>
             Valor MÁXIMO do desconto: R${item.descTotal123?.toFixed(2)}<br/>
          Valor a pagar: R$ <strong>{item.valorPay123?.toFixed(2)}</strong><br/>
-         Valor do boleto: R$ <strong>{item.valorBol123?.toFixed(2)}</strong>
         </div>
         }
                     
-         {item.title === 'Faixa 2' &&
+        {item.title === 'Faixa 2' && (
         <div className={styles.yorImc}>
-            Valor MÁXIMO do desconto: R${item.descTotal123?.toFixed(2)}<br/>
-         Valor a pagar: R$ <strong>{item.valorPay123?.toFixed(2)}</strong><br/>
-         Valor do boleto: R$ <strong>{item.valorBol123?.toFixed(2)}</strong>
-        </div>
-        }
+            {item.validPay !== undefined && item.validPay < 50 ? (
+                    <p>
+                        Valor MÁXIMO do desconto: R${item.descP50?.toFixed(2)}<br/>
+                        Valor a pagar: R$ <strong>50,00</strong><br/>
+                    </p>
+                    ) : ( 
+                    <p>
+                        Valor MÁXIMO do desconto: R${item.descTotal123?.toFixed(2)}<br/>        
+                        Valor a pagar: R$ <strong>{item.valorPay123?.toFixed(2)}</strong><br/>
+                    </p>
+                    )}
+            </div>
+            )}
                     
          {item.title === 'Faixa 3' &&
         <div className={styles.yorImc}>
          Valor MÁXIMO do desconto: R${item.descTotal123?.toFixed(2)}<br/>
          Valor a pagar: R$ <strong>{item.valorPay123?.toFixed(2)}</strong><br/>
-         Valor do boleto: R$ <strong>{item.valorBol123?.toFixed(2)}</strong>
         </div>
         }
                     
