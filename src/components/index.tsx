17db59612b503041f411e65e1c01164be5434d77
Jonathas Nunes <jonathas.ns1509@gmail.com>
2024-06-06 08:16:00 -0300

    Update index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -53,7 +53,7 @@ export const GridItem = ({ item }: Props) => {
                     
         {item.title === 'Faixa 1' &&
         <div className={styles.yorImc}>
-            Valor MÁXIMO do desconto: R${item.descTotal123?.toFixed(2)}<br/>
+           t Valor MÁXIMO do desconto: R${item.descTotal123?.toFixed(2)}<br/>
          Valor a pagar: R$ <strong>{item.valorPay123?.toFixed(2)}</strong><br/>
         </div>
         }
